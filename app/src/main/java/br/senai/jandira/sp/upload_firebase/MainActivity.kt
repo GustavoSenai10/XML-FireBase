3958b7d680ac223c29d7bf88ba903bade8cdf90e
GUSTAVO SOUZA TENORIO DE BARROS <22282225@dsn127m1246730.cfp127aluno.local>
2023-09-22 16:57:01 -0300

    XML FireBase:Inicio

---
+++ app/src/main/java/br/senai/jandira/sp/upload_firebase/MainActivity.kt
@@ -0,0 +1,138 @@
+package br.senai.jandira.sp.upload_firebase
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import br.senai.jandira.sp.upload_firebase.databinding.ActivityMainBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+class MainActivity : AppCompatActivity() {
+
+    //Atributos
+    //Representação da classe de manipulação de objetos de view das telas
+    private  lateinit var binding: ActivityMainBinding
+
+    //Representação da classe de manipulação de endereço (Local) de arquivos
+    private  var imageUri: Uri? = null
+
+    //Referencia para acesso e manipulação do Cloud Storage
+    private lateinit var storeRef: StorageReference
+
+
+    //Referencia para acesso e manipulação do cloud FireStore
+    private lateinit var firebaseStorage: FirebaseStorage
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initVars()
+        registerClickEvents()
+    }
+
+    //Inicialização dos Atributos do FireBase
+    private fun initVars(){
+        storeRef = FirebaseStorage.getInstance().reference.child("imagens")
+        firebaseStorage =FirebaseStorage.getInstance()
+    }
+
+    private val resultLaucher = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){
+        imageUri = it
+        binding.imageView.setImageURI(it)
+    }
+
+    //Tratamento de evento click
+    private fun registerClickEvents(){
+        binding.imageView.setOnClickListener{
+            resultLaucher.launch("image/*")
+        }
+        binding.uploadBtn.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    //UPLAOD DE IMAGENS NO FIREBASE
+    private fun uploadImage(){
+
+        binding.progressBar.visibility= View.VISIBLE
+
+        storeRef = storeRef.child(System.currentTimeMillis().toString())
+
+        //Uplaod V1 - Inicio
+//        imageUri?.let {
+//
+//            storeRef.putFile(it).addOnCompleteListener{
+//
+//                teks->
+//
+//                if (teks.isSuccessful){
+//                    Toast.makeText(
+//                        this,
+//                        "UPLOAD REALIZADO COM SUCESSO!",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }else{
+//                    Toast.makeText(
+//                        this,
+//                        "HOUVE UM ERRO AO TENTAR REALIZAR O UPLOAD!",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//                binding.progressBar.visibility= View.GONE
+//            }
+//        }
+//        //Uplaod V1 - FIM
+
+
+        //Uplaod V2 - Inicio
+        imageUri?.let {
+            storeRef.putFile(it).addOnCompleteListener { task->
+
+                if (task.isSuccessful) {
+
+                    storeRef.downloadUrl.addOnSuccessListener { uri ->
+
+                        val map = HashMap<String, Any>()
+                        map["pic"] = uri.toString()
+
+                        firebaseFirestore.collection("images").add(map).addOnCompleteListener { firestoreTask ->
+
+                            if (firestoreTask.isSuccessful){
+                                Toast.makeText(this, "Uploaded Successfully", Toast.LENGTH_SHORT).show()
+
+                            }else{
+                                Toast.makeText(this, firestoreTask.exception?.message, Toast.LENGTH_SHORT).show()
+
+                            }
+                            binding.progressBar.visibility = View.GONE
+                            binding.imageView.setImageResource(R.drawable.vector)
+
+                        }
+                    }
+
+                }else{
+
+                    Toast.makeText(this,  task.exception?.message, Toast.LENGTH_SHORT).show()
+
+                }
+
+                //BARRA DE PROGRESSO DO UPLOAD
+                binding.progressBar.visibility = View.GONE
+
+                //TROCA A IMAGEM PARA A IMAGEM PADRÃO
+                binding.imageView.setImageResource(R.drawable.vector)
+
+            }
+
+    }
+
+}
